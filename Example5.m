fe24876aa231c6dc58f47fa6fa7d810c8a343567
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2023-02-21 23:16:47 +0300

    Update Example5.m

--- Example5.m
+++ Example5.m
@@ -1,12 +1,3 @@
-% Affan Selim Kaya 
-% 200709006
-% Computer Engineering (Eng)
-% Introduction to Programming 
-% CEN103
-% 2020-2021 1st Somestr
-% Project 1 Example 5
-% Asst.Prof. Deniz Dal
-
 salaryAmount = input("Your monthly income = ");
 publicEmployee = input("Are you a public employee = ", "s");
 
